12e4c2fa04a59253ed79f278b12727f71f48e961
Doug Clinton <doug@wellfactored.com>
2016-08-31 16:12:57 +0100

    Build links for the root call response

--- src/main/scala/uk.gov.bis/levyApiMock/controllers/api/RootController.scala
+++ src/main/scala/uk.gov.bis/levyApiMock/controllers/api/RootController.scala
@@ -13,15 +13,22 @@ import scala.concurrent.ExecutionContext
 case class RootResponse(_links: Map[String, Href], emprefs: Seq[String])
 
 class RootController @Inject()(users: GatewayUserOps, authenticatedAction: AuthenticatedAction)(implicit ec: ExecutionContext) extends Controller {
-
   implicit def hrefW = Json.writes[Href]
 
   implicit def rootW = Json.writes[RootResponse]
 
   def root = authenticatedAction.async { request =>
     users.forGatewayID(request.authRecord.gatewayID).map {
-      case Some(user) => Ok(Json.toJson(RootResponse(Map(), Seq(user.empref))))
+      case Some(user) => Ok(Json.toJson(RootResponse(buildLinks(user.empref), Seq(user.empref))))
       case _ => NotFound
     }
+  }
+
+  def buildLinks(empref: String): Map[String, Href] = {
+    import views.html.helper.urlEncode
+    Map(
+      "self" -> Href("/"),
+      "empref" -> Href(s"/epaye/${urlEncode(empref)}")
+    )
   }
 }
