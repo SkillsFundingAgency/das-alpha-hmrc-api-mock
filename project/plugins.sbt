ae2bf8a6b989e0f66ee7b1c8f4cc269d3ac83fe6
Doug Clinton <doug@wiredthing.com>
2016-12-19 14:18:19 +0000

    Added healthcheck endpoints

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,9 +5,10 @@ addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.9")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5")
 
-
 resolvers += "Typesafe Repository" at "https://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
 
 addSbtPlugin("com.codacy" % "sbt-codacy-coverage" % "1.3.7")
+
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.6.1")
---
+++ src/main/scala/uk.gov.bis/levyApiMock/controllers/HealthCheckController.scala
@@ -0,0 +1,17 @@
+package uk.gov.bis.levyApiMock.controllers
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import uk.gov.bis.levyApiMock.buildinfo.BuildInfo
+
+class HealthCheckController extends Controller {
+  def ping = Action {
+    Ok("alive")
+  }
+
+  def version = Action {
+    // need to convert the Anys to Strings so play json knows how to
+    // convert it
+    Ok(Json.toJson(BuildInfo.toMap.mapValues(_.toString)))
+  }
+}
